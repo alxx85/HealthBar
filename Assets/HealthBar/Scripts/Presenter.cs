d0cb1d7ba606809857b8e68f184315d6bea7e784
alxx85 <alxx85@mail.ru>
2022-01-27 19:57:34 +0700

    ...

---
+++ Assets/HealthBar/Scripts/Presenter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Presenter : MonoBehaviour
+{
+    [SerializeField] private Slider _sliderHP;
+    [SerializeField] private float _changeSpeed;
+
+    private float _currentHealth = 50f;
+
+
+    private void Start()
+    {
+        _sliderHP.value = _currentHealth;
+    }
+
+    private void Update()
+    {
+        if (_currentHealth != _sliderHP.value)
+        {
+            _sliderHP.value = Mathf.MoveTowards(_sliderHP.value, _currentHealth, _changeSpeed * Time.deltaTime);
+        }
+    }
+
+    public void ChangeHealth(float value)
+    {
+        _currentHealth += value;
+    }
+}
